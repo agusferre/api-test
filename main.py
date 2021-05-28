d2bfb6224fc679b3e726a7abf27c27bb05f84613
nitsu <agusferrex@gmail.com>
2021-05-27 21:15:17 -0300

    check reqs

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def test():
-    return 'Hello wo'
+class test(Resource):
+    def get(self):
+        return {'data': 'hey dog'}
+
+    def post(self):
+        return {'data': 'heya'}
+
+api.add_resource(test, '/doggy')
+
+if __name__ == '__main__':
+    app.run(debug=True)
