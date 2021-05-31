fb59c31775b94ac33b2108b49aa36cec38c24923
Agus Ferré <agusferrex@gmail.com>
2021-05-31 20:25:35 -0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-BASE = 'http://127.0.0.1:5000/'#'https://api-test-01010101.uc.r.appspot.com/'
-
-#response = requests.post(BASE + 'traces', {'ip': '57.11.255'})
-response = requests.get(BASE + 'statistics')
-print(response.json())
